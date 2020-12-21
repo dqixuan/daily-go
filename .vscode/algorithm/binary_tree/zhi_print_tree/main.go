ca894b4f32c62cdcebfb2bd190a589df691013a0
dqixuan <654182528@qq.com>
2020-12-21 22:37:49 +0800

    chore: zhi print tree

---
+++ .vscode/algorithm/binary_tree/zhi_print_tree/main.go
@@ -0,0 +1,112 @@
+package main
+
+import "fmt"
+
+/*
+请实现一个函数按照之字形打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印，其他行以此类推。
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+*/
+
+func main() {
+	root := TreeNode{
+		Val: 0,
+		Left: nil,
+		Right: nil,
+	}
+	n1 := TreeNode{
+		Val: 1,
+		Left: nil,
+		Right: nil,
+	}
+	n2 := TreeNode{
+		Val: 2,
+		Left: nil,
+		Right: nil,
+	}
+	n3 := TreeNode{
+		Val: 3,
+		Left: nil,
+		Right: nil,
+	}
+	n4 := TreeNode{
+		Val: 4,
+		Left: nil,
+		Right: nil,
+	}
+	n5 := TreeNode{
+		Val: 5,
+		Left: nil,
+		Right: nil,
+	}
+	root.Left = &n1
+	root.Right = &n2
+	n1.Left = &n3
+	n1.Right = &n4
+	n3.Right = &n5
+
+	reult := Print(&root)
+	fmt.Println(reult)
+}
+
+// TreeNode 节点结构
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// Print 打印之字型输出 待优化
+func Print( pRoot *TreeNode ) [][]int {
+	idx := 0
+	var result [][]int
+	if pRoot == nil {
+		return result
+	}
+	root := []*TreeNode{pRoot}
+	result = append(result, []int{pRoot.Val})
+	for {
+		var (
+			s []int
+			ts []*TreeNode
+		)
+		if idx %2 != 0 {
+			for _, v := range root {
+				if v.Left != nil {
+					ts = append(ts, v.Left)
+					s = append(s, v.Left.Val)
+				}
+				if v.Right != nil {
+					ts = append(ts, v.Right) 
+					s = append(s, v.Right.Val)
+				}
+			}
+		} else {
+			for i:= len(root) - 1; i >= 0; i-- {
+				tmp := root[i]
+				if tmp.Right != nil {
+					// ts = append(ts, tmp.Right) 
+					ts = append([]*TreeNode{tmp.Right}, ts...)
+					s = append(s, tmp.Right.Val)
+				}
+				if tmp.Left != nil {
+					ts = append([]*TreeNode{tmp.Left}, ts...)
+					s = append(s, tmp.Left.Val)
+				}
+			}
+		}
+		if len(s) != 0 {
+			result = append(result, s)
+		}
+		if len(ts) == 0 {
+			break
+		}
+		root = ts
+		idx++
+	}
+	return result
+}
