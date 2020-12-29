595300f1d8cbacbaf8598dec56b4e3f6f8f32069
dqixuan <654182528@qq.com>
2020-12-29 14:05:24 +0800

    chore: leetcode26

---
+++ algorithm/leetcode26/leetcode26.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+/*
+
+给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+示例 1:
+
+给定数组 nums = [1,1,2], 
+
+函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。 
+
+你不需要考虑数组中超出新长度后面的元素。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+
+func removeDuplicates(numbers []int) int {
+	length := len(numbers)
+	if length== 0 || length == 1 {
+		return length
+	}
+	var (
+		pre = 0
+		cur = 1
+	)
+	for cur < length {
+		if numbers[pre] == numbers[cur] {
+			cur++
+		} else {
+			if pre +1 != cur {
+				numbers[pre+1] = numbers[cur]
+			}
+			pre++
+			cur++
+		}
+	}
+	fmt.Println(numbers)
+	return pre+1
+}
+
+func main() {
+	fmt.Println(removeDuplicates([]int{1, 2}))
+	fmt.Println(removeDuplicates([]int{1, 1, 2}))
+}
---
+++ algorithm/leetcode80/leetcode80.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+
+给定一个增序排列数组 nums ，你需要在 原地 删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+示例 1：
+
+输入：nums = [1,1,1,2,2,3]
+输出：5, nums = [1,1,2,2,3]
+解释：函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3 。 你不需要考虑数组中超出新长度后面的元素。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+
+func removeDuplicates(nums []int) int {
+
+}
+
+func main() {
+	
+}
