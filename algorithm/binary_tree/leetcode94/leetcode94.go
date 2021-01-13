0dd944fe06d20bec160686066ce9dc95457dc62d
dqx <654182528@qq.com>
2021-01-13 23:16:46 +0800

    chore: inorder traversal

--- algorithm/binary_tree/leetcode94/leetcode94.go
+++ algorithm/binary_tree/leetcode94/leetcode94.go
@@ -33,8 +33,18 @@ func inOrderTraversal(root *TreeNode) (res []int) {
 func inorderTraversal(root *TreeNode) []int {
 	ans := []int{}
 	stack := []*TreeNode{}
-	
-	return nil
+
+	for root != nil || len(stack) != 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack) - 1]
+		stack = stack[:len(stack) - 1]
+		ans = append(ans, root.Val)
+		root = root.Right
+	}
+	return ans
 }
 
 
