a35b1b1c41d06c1c429226a26bbab59fe49c4204
dqixuan <654182528@qq.com>
2021-02-23 16:35:56 +0800

    chore: leetcode 78

--- algorithm/leetcode46/leetcode46.go
+++ algorithm/leetcode46/leetcode46.go
@@ -1,4 +1,67 @@
-package leetcode46
+package main
+
+import "fmt"
 
 // 46 全排列
 // 给定一个 没有重复 数字的序列，返回其所有可能的全排列
+
+// permute1 方法1：交换法
+func permute1(nums []int) [][]int {
+	ans := make([][]int, 0)
+	if len(nums) == 0 {
+		return ans
+	}
+	if len(nums) == 1 {
+		ans = append(ans, nums)
+		return ans
+	}
+	backTracing(nums, 0, &ans)
+	return ans
+}
+
+func backTracing(nums []int, idx int, ans *[][]int) {
+	*ans = append(*ans, append([]int{}, nums...))
+	for i, l := idx, len(nums); i < l-1; i++ {
+		for j := i + 1; j < l; j++ {
+			nums[i], nums[j] = nums[j], nums[i]
+			backTracing(nums, i+1, ans)
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+	}
+	return
+}
+
+// permute  方法2：遍历法
+func permute(nums []int) [][]int {
+	var (
+		arr  []int
+		ans  [][]int
+		isIn = make([]bool, len(nums))
+	)
+	helper(nums, isIn, &arr, &ans)
+	return ans
+}
+
+func helper(nums []int, isIn []bool, arr *[]int, ans *[][]int) {
+	if len(*arr) == len(nums) {
+		*ans = append(*ans, append([]int{}, *arr...))
+		return
+	}
+	for i, l := 0, len(nums); i < l; i++ {
+		if isIn[i] == true {
+			continue
+		}
+		*arr = append(*arr, nums[i])
+		isIn[i] = true
+		helper(nums, isIn, arr, ans)
+		*arr = (*arr)[:len(*arr)-1]
+		isIn[i] = false
+	}
+	return
+}
+
+func main() {
+	nums := []int{1, 2, 3}
+	fmt.Println(permute(nums))
+	fmt.Println(permute1(nums))
+}
---
+++ algorithm/leetcode78/leetcode78.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+/**
+leetcode 78 子集
+给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+*/
+
+func subsets(nums []int) [][]int {
+	var (
+		ans [][]int
+		arr []int
+	)
+	if len(nums) == 0 {
+		return ans
+	}
+	helper(nums, 0, &arr, &ans)
+	return ans
+}
+
+func helper(nums []int, idx int, arr *[]int, ans *[][]int) {
+	*ans = append(*ans, append([]int{}, *arr...))
+
+	for i, l := idx, len(nums); i < l; i++ {
+		*arr = append(*arr, nums[i])
+		helper(nums, i+1, arr, ans)
+		*arr = (*arr)[:len(*arr)-1]
+	}
+	return
+}
+
+func main() {
+	nums := []int{1, 2, 3}
+	fmt.Println(subsets(nums))
+}
