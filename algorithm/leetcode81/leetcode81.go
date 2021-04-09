8fa7c362f9a2f188507936ec107343c2a77f43f5
dqx <654182528@qq.com>
2021-04-09 21:45:34 +0800

    chore

--- algorithm/leetcode81/leetcode81.go
+++ algorithm/leetcode81/leetcode81.go
@@ -1,14 +1,29 @@
 package main
 
 func search(nums []int, target int) bool {
-	left, right := 0, len(nums)-1
+	if len(nums) == 0 {
+		return false
+	}
+	left, right := 0, len(nums) - 1
 	for left <= right {
-		mid := left + (right-left)>>1
+		mid := left + (right - left) >> 1
 		if nums[mid] == target {
 			return true
-		} else if nums[mid] > target {
-			if nums[mid] <= nums[right] {
-				right = mid-1
+		}
+		if nums[mid] == nums[left] && nums[mid] == nums[right] {
+			left++
+			right--
+		} else if nums[left] <= nums[mid] {
+			if target >= nums[left] && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if target > nums[mid] && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
 			}
 		}
 	}
