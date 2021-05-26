1c8c2385445d94990172a38ec6af0f9f4dea27bd
dqx <654182528@qq.com>
2021-05-26 23:09:00 +0800

    chore: majorityElement

---
+++ algorithm/Palindrome/palindrome.go
@@ -0,0 +1,21 @@
+package Palindrome
+
+import "strconv"
+
+/**
+	回文数  121 true  10 false
+	leetcode 9
+ */
+
+func isPalindrome(x int) bool {
+	s := strconv.Itoa(x)
+	left, right := 0, len(s)-1
+	for left < right {
+		if s[left] != s[right] {
+			return false
+		}
+		left++
+		right--
+	}
+	return true
+}
