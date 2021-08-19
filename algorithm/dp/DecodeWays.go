07ae687ba46ddb093449082c3ce4d83b9fc00aaf
dqixuan <654182528@qq.com>
2021-08-19 14:13:27 +0800

    decodeways

--- algorithm/dp/DecodeWays.go
+++ algorithm/dp/DecodeWays.go
@@ -28,3 +28,36 @@ func numDecodings(s string) int {
 	return dp[len(s)]
 }
 
+/**
+	自己的理解：
+
+ */
+func numDecodings2(s string) int {
+	length := len(s)
+	dp := make([]int, length)
+	for i:=0; i < length; i++ {
+		if s[i] - '0' >= 1 && s[i] - '0' <= 9 {
+			if i > 0 {
+				dp[i] = dp[i-1]
+			} else {
+				dp[i]=1
+			}
+		}
+		if i > 0 {
+			if s[i-1] - '0' > 0 && (s[i-1] - '0') * 10+s[i] - '0' <= 26  {
+				if i-2>=0 {
+					// "11 23"  中间的两位
+					dp[i]+=dp[i-2]
+				} else {
+					// "11" 最开始的两位
+					dp[i] += 1
+				}
+			}
+
+		}
+	}
+	return dp[length-1]
+}
+
+
+
